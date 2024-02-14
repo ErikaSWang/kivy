2f95db573475e334c1b15c995d6eb558fbd58990
ErikaSWang <nibby_meh@yahoo.com>
2024-02-14 01:13:27 -0500

    mobile apps using kivy for python

--- my_kivy_project/project_helloWorld/main.py
+++ my_kivy_project/project_helloWorld/main.py
@@ -1,15 +1,23 @@
-import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
-
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 
-
-class MyApp(App):
-
+class MainApp(App):
     def build(self):
-        return Label(text='Hello world')
+        # Items are called 'WIDGETS' in Kivy
+        label = Label(text='Hello from Kivy', # The words to display 
+                    size_hint=(.5, .5),   # This seems to have no effect whatsoever??
+                                        # Notes say this is the proportion of distortion??
+                                        # Default is (1,1) ??
+                    pos_hint={'center_x': .5, 'center_y': .05}) # Looks like this is (x,y)
+                                                                # (0,0) is bottom left, (1,1) is top right?
 
+        img = Image(source='./love.jpg',
+                    size_hint=(.25, .25),
+                    pos_hint={'center_x':.5, 'center_y':.5})
+
+        return img; label #unable to show both label and image at the same time??
 
 if __name__ == '__main__':
-    MyApp().run()
+    app = MainApp()
+    app.run()
